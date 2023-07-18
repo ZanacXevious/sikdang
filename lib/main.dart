ec0a95bc93d7dadd8d94ab416fe7eba9bba9bad0
ZanacXevious <ckongmario21@outlook.com>
2023-07-17 21:16:54 -0600

    02 TextField Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sikdang/common/const/colors.dart';
+import 'common/component/custom_text_form_field.dart';
 
 void main() {
   runApp(_App());
@@ -12,7 +14,21 @@ class _App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (String value) {},
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요.',
+              onChanged: (String value) {},
+              obscureText: true,
+            ),
+          ],
+        ),
       ),
     );
   }
